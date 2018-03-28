02f01db81a6c5d7e151da16d818fad9d6c0b5e42
cesarfuster <cfuster@uol.com.br>
2018-03-28 14:39:53 -0300

    all done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+bristol = Restaurant.create(name: "Epicure", address: "75008 Paris", category: "french")
+bristol2 = Restaurant.create(name: "Epicure2", address: "75010 Paris", category: "french")
+china = Restaurant.create(name: "China in Box", address: "75010 Brasil", category: "chinese")
+andiamo = Restaurant.create(name: "Andiamo", address: "1020 Italy", category: "italian")
+supera = Restaurant.create(name: "Super Italy", address: "10 Italy", category: "italian")
+sushi = Restaurant.create(name: "Sushi Maravilha", address: "010 Japan", category: "japanese")
+belgian = Restaurant.create(name: "Belga mix", address: "5010 Belgium", category: "belgian")
